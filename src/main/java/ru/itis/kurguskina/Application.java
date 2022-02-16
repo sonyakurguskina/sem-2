5be6321b25e5a9ad75084c8f9f68026996d164f0
sonyakurguskina <sonyakurguskinaa@gmail.com>
2022-02-16 11:40:46 +0300

    second task

--- src/main/java/ru/itis/kurguskina/Application.java
+++ src/main/java/ru/itis/kurguskina/Application.java
@@ -2,6 +2,7 @@ package ru.itis.kurguskina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +10,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//        return bCryptPasswordEncoder();
+//    }
 }
--- src/main/java/ru/itis/kurguskina/controller/Weather.java
+++ src/main/java/ru/itis/kurguskina/controller/Weather.java
@@ -1,24 +1,59 @@
 package ru.itis.kurguskina.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.itis.kurguskina.advice.ParseHelper;
+import ru.itis.kurguskina.dto.UserDto;
+import ru.itis.kurguskina.repository.UserRepository;
+import ru.itis.kurguskina.repository.WeatherRepository;
 import ru.itis.kurguskina.service.WeatherHttpService;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
 public class Weather {
     private WeatherHttpService weatherHttpService = new WeatherHttpService();
+    private final UserRepository userRepository;
+    private final WeatherRepository weatherRepository;
+    private final static ParseHelper parseHelper = new ParseHelper();
+//@Autowired
+//private BCryptPasswordEncoder encoder;
+
+    @Autowired
+    public Weather(UserRepository userRepository, WeatherRepository weatherRepository) {
+        this.userRepository = userRepository;
+        this.weatherRepository = weatherRepository;
+}
+
+    public Weather(String description, String humidity, String temp, String name, String userEmail, UserRepository userRepository, WeatherRepository weatherRepository) {
+        this.userRepository = userRepository;
+        this.weatherRepository = weatherRepository;
+    }
 
     @GetMapping("/result")
-    public String hello(@RequestParam Optional<String> city) throws IOException {
-        String result = weatherHttpService.get(city.orElse("Kazan"));
-        if (result != null){
-            return result;
+    public String getWeather(@RequestParam Optional<String> city, @RequestParam Optional<String> email)
+            throws IOException {
+        String userEmail = email.orElse("null");
+        UserDto userDto = userRepository.findByEmail(userEmail);
+
+        if (userDto != null) {
+            String result = weatherHttpService.get(city.orElse("Kazan"));
+
+            if (result != null) {
+                Map<String, String> params = parseHelper.parseJson(result);
+                weatherRepository.save(new Weather(params.get("description"), params.get("humidity"),
+                        params.get("temp"), params.get("name"), userEmail, userRepository, weatherRepository));
+
+                return result;
+            } else {
+                return "not found";
+            }
         } else {
-            return "not found";
+            return "null";
         }
 
     }
